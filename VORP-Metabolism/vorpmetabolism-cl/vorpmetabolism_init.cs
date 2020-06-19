eeb2c457c2c52f05d684a0df27c860a5fdfc073c
Osmany González <devbiitz2@gmail.com>
2020-06-19 15:33:06 +0100

    Events Init & Save Status

--- VORP-Metabolism/vorpmetabolism-cl/vorpmetabolism_init.cs
+++ VORP-Metabolism/vorpmetabolism-cl/vorpmetabolism_init.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CitizenFX.Core;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace vorpmetabolism_cl
 {
-    public class vorpmetabolism_init
+    public class vorpmetabolism_init : BaseScript
     {
+        public static JObject pStatus = new JObject();
+
+        public vorpmetabolism_init()
+        {
+            EventHandlers["vorpmetabolism:StartFunctions"] += new Action<string>(StartFunctions);
+        }
+
+        private void StartFunctions(string status)
+        {
+            pStatus = JObject.Parse(status);
+        }
     }
 }
--- VORP-Metabolism/vorpmetabolism-sv/vorpmetabolism_init.cs
+++ VORP-Metabolism/vorpmetabolism-sv/vorpmetabolism_init.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CitizenFX.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,45 @@ using System.Threading.Tasks;
 
 namespace vorpmetabolism_sv
 {
-    public class vorpmetabolism_init
+    public class vorpmetabolism_init : BaseScript
     {
-        
+        public static Dictionary<Player, string> lastPlayerStatus = new Dictionary<Player, string>();
+
+        public vorpmetabolism_init()
+        {
+            EventHandlers["vorpmetabolism:SaveLastStatus"] += new Action<Player, string>(SaveLastStatus);
+            EventHandlers["playerDropped"] += new Action<Player, string>(OnPlayerDropped);
+
+            Tick += saveLastStatusTick;
+        }
+
+        private void OnPlayerDropped(Player player, string reason)
+        {
+            string sid = ("steam:" + player.Identifiers["steam"]);
+            if (lastPlayerStatus.ContainsKey(player))
+            {
+                Exports["ghmattimysql"].execute("UPDATE characters SET status=? WHERE identifier=?", new[] { lastPlayerStatus[player], sid });
+            }
+        }
+
+        private void SaveLastStatus(Player player, string status)
+        {
+            lastPlayerStatus[player] = status;
+        }
+
+        [Tick]
+        public async Task saveLastStatusTick()
+        {
+            await Delay(300000);
+            foreach (var playerStatus in lastPlayerStatus)
+            {
+                string sid = ("steam:" + playerStatus.Key.Identifiers["steam"]);
+                try
+                {
+                    Exports["ghmattimysql"].execute("UPDATE characters SET status=? WHERE identifier=?", new[] { playerStatus.Value, sid });
+                }
+                catch { continue; }
+            }
+        }
     }
 }
