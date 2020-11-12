ae5812d6d6255fce5ba066449faeb45387a28ef0
Chip Wickings <34577860+Chip-W@users.noreply.github.com>
2020-11-12 06:18:57 -0500

    English SQL

---
+++ VORP-Metabolism/build/vorp_metabolism/loadInYourDB_En.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_coffee', 'Coffee', 5, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_haycube', 'Hay', 50, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_kidneybeans_can', 'Can of Beans', 5, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_medicine', 'Medicine', 3, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_peach', 'Peach', 5, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_salmon_can', 'Can of Salmon', 5, 1, 'item_standard', 1);
